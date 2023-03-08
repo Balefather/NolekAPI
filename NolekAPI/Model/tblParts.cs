96faabfcaa6a66bf84653c5ff23d477ca292298e
Bastian Krogh Grauenkjær <bkg@digizuite.com>
2023-03-08 13:30:52 +0100

    part price is now double

--- NolekAPI/Model/tblParts.cs
+++ NolekAPI/Model/tblParts.cs
@@ -7,6 +7,6 @@ namespace NolekAPI.Model
         public int PartID { get; set; }
         public string PartName { get; set; }
         public int NumberInStock { get; set; }
-        public int PartPrice { get; set; }
+        public double PartPrice { get; set; }
     }
 }
