9c54b7fb8aa9b46a4d1476677103f063048990d3
Nicklas Jacobsen <Horm@live.dk>
2023-04-12 13:28:39 +0200

    O

--- NolekAPI/Model/Invoice.cs
+++ NolekAPI/Model/Invoice.cs
@@ -5,11 +5,11 @@
         public DateTime ServiceDate { get; set; }
         public string TotalPartsName { get; set; }
         public int WorkTimeUsed { get; set; }
-        public decimal WorkPrice { get; set; }
+        public double WorkPrice { get; set; }
         public int TransportTimeUsed { get; set; }
-        public decimal TransportTimePrice { get; set; }
+        public double TransportTimePrice { get; set; }
         public int TransportKmUsed { get; set; }
-        public decimal TransportKmPrice { get; set; }
-        public decimal TotalPrice { get; set; }
+        public double TransportKmPrice { get; set; }
+        public double TotalPrice { get; set; }
     }
 }
