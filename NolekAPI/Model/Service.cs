8d82a593d767ea5817571448dcec11cf5624a6d9
Nicklas Jacobsen <Horm@live.dk>
2023-04-12 10:49:49 +0200

    Service class fixed

--- NolekAPI/Model/Service.cs
+++ NolekAPI/Model/Service.cs
@@ -6,12 +6,18 @@ namespace NolekAPI.Model
     {
         [Key]
         public int ServiceID { get; set; }
-        public int CustomerID { get; set; }
-        public int MachineID { get; set; }
         public DateTime ServiceDate { get; set; }
+        public string CustomerName { get; set; }
+        public string MachineName { get; set; }
+        public string MachineSerialNumber { get; set; }
+        public int PartsUsed { get; set; }
+        public string PartName { get; set; }
         public int TransportTimeUsed { get; set; }
         public int TransportKmUsed { get; set; }
         public int WorkTimeUsed { get; set; }
         public string ServiceImage { get; set; }
+        public string Note { get; set; }
+        public string MachineStatus { get; set; }
     }
+    
 }
